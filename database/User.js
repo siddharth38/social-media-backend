b57a513ca7c465f9ae627003795750e9cb7864b0
Siddharth Kumar <siddharth.kumar28717@gmail.com>
2024-01-21 16:56:25 +0530

    updated server

--- database/User.js
+++ database/User.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: String,
-  phone: String,
-  profession: String,
-  dob: String,
-  password: String,
-  follows: {
-    type: [],
+const UserSchema = new Schema(
+  {
+    googleId: String, // adding top 3 new fields for google authentication and followCount;
+    email: String,
+    followCount: Number,
+    name: String,
+    phone: String,
+    profession: String,
+    dob: String,
+    password: String,
+    follows: {
+      type: [],
+    },
   },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,57 +1,64 @@
 const router = require("express").Router();
-const User = require('../database/User');
+const User = require("../database/User");
 const bcrypt = require("bcrypt");
-const  jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 //login controller
 router.post("/login", (req, res) => {
-    const { name, password } = req.body
-	console.log('called')
-    User.findOne({ name: name }, async (err, user) => {
-        if (user) {
-            const isPasswordValid = await bcrypt.compare(password, user.password);
-            if (isPasswordValid) {
-                const token = jwt.sign(
-                    {  name: user.name, userId:  user._id },
-                    "ajw065123",
-                    {
-                      expiresIn: "1h",
-                    }
-                  );
-              
-                res.send({ message: "Login Successfull",name: user.name, userId:  user._id ,token })
+  const { name, password } = req.body;
+  console.log("called");
+  User.findOne({ name: name }, async (err, user) => {
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (isPasswordValid) {
+        const token = jwt.sign(
+          { name: user.name, userId: user._id },
+          "ajw065123",
+          {
+            expiresIn: "1h",
+          }
+        );
 
-            } else {
-                res.send({ message: "Password didn't match" })
-            }
-        } else {
-            res.send({ message: "User not registered" })
-        }
-    })
-})
+        res.send({
+          message: "Login Successfull",
+          name: user.name,
+          userId: user._id,
+          token,
+        });
+      } else {
+        res.send({ message: "Password didn't match" });
+      }
+    } else {
+      res.send({ message: "User not registered" });
+    }
+  });
+});
 // register controller
 router.post("/register", (req, res) => {
-    const { name, password ,profession , dob , phone } = req.body
-    User.findOne({ name: name }, async (err, user) => {
-        if (user) {
-            res.send({ message: "User already registered" })
+  const { name, password, profession, dob, phone } = req.body;
+  User.findOne({ name: name }, async (err, user) => {
+    if (user) {
+      res.send({ message: "User already registered" });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({
+        name,
+        phone,
+        dob,
+        profession,
+        password: hashedPassword,
+      });
+      user.save((err) => {
+        if (err) {
+          res.send({ message: "Error in login" });
         } else {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const user = new User({
-                name,
-                phone ,
-                dob ,
-                profession ,
-                password: hashedPassword,
-            })
-            user.save(err => {
-                if (err) {
-                    res.send({message : "Error in login"})
-                } else {
-                    res.send({ message: "Successfully Registered, Please login now.", userId: user._id, name: user.name })
-                }
-            })
+          res.send({
+            message: "Successfully Registered, Please login now.",
+            userId: user._id,
+            name: user.name,
+          });
         }
-    })
-
-})
+      });
+    }
+  });
+});
 module.exports = router;
